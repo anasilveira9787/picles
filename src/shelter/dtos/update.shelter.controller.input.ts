5ec4655c58ce65523ff58a3fa5cd377d73207a64
anasilveira9787 <anacarolina.silveira@rocketseat.team>
2024-04-03 22:06:58 -0300

    added validators

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsNumberString, IsString, Length } from "class-validator"
+
 export default class UpdateShelterControllerInput {
+  @IsNotEmpty()
+  @IsString()
   name: string
+  @IsNotEmpty()
+  @IsString()
+  @Length(10,11)
   whatsapp: string
+  @IsNotEmpty()
+  @IsString()
+  @IsNumberString()
   phone: string
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string
 }
